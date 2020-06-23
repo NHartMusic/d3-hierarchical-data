2b0f2b9ad05a827f707a960de626133ee16601b4
Nick Hart <nhartmusic@gmail.com>
2020-06-23 17:36:20 +0200

    tree project complete and final project is done yayyy

--- graph.js
+++ graph.js
@@ -1,4 +1,4 @@
-const dimensions = { height: 500, width: 1100 }
+const dimensions = { height: 500, width: 900 }
 
 const svg = d3.select('.canvas')
     .append('svg')
@@ -16,17 +16,41 @@ const stratify = d3.stratify()
 const tree = d3.tree()
     .size([dimensions.width, dimensions.height])
 
+//create ordinal scale
+
+const colour = d3.scaleOrdinal(['#CAA8F5', '#9984D4', '#592E83', '#230C33'])
+
 //update function
 const update = (data) => {
+    //remove current nodes
+    graph.selectAll('.node').remove()
+    graph.selectAll('.link').remove()
+
+    //update ordinal scale domain
+    colour.domain(data.map(item => item.department))
 
     //get updated root node data
     const rootNode = stratify(data)
-
     const treeData = tree(rootNode)
 
     //get nodes selection and join data
     const nodes = graph.selectAll('.node')
         .data(treeData.descendants())
+
+    //get link selection and join data
+    const links = graph.selectAll('.link')
+        .data(treeData.links())
+
+    links.enter()
+        .append('path')
+        .attr('class', 'link')
+        .attr('fill', 'none')
+        .attr('stroke', 'white')
+        .attr('stroke-width', 2)
+        .attr('d', d3.linkVertical()
+            .x(d => d.x)
+            .y(d => d.y)
+        )
 
     const enterNodes = nodes.enter()
         .append('g')
@@ -35,11 +59,15 @@ const update = (data) => {
 
     //append rects to enter nodes
     enterNodes.append('rect')
-        .attr('fill', '#aaa')
-        .attr('stroke', '#555')
+        .attr('fill', d => colour(d.data.department))
+        .attr('stroke', 'white')
         .attr('stroke-width', 2)
         .attr('height', 50)
         .attr('width', d => d.data.name.length * 20)
+        .attr('transform', d => {
+            let x = d.data.name.length * 10
+            return `translate(${-x}, -25)`
+        })
 
     //append name text 
     enterNodes.append('text')
